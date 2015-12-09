c6c5206e640ff700408886f49782b69b516771a3
Jack_McKing <JackMcKing@users.noreply.github.com>
2015-12-10 05:21:31 +0800

    增加Mdb数据库,存放聊天信息
    courier的聊天activity = SendMessage.class
    
    user的聊天activity = Recieve.class

--- app/src/main/java/com/example/wangchenguang/thelastmile/Db.java
+++ app/src/main/java/com/example/wangchenguang/thelastmile/Db.java
@@ -1,7 +1,30 @@
 package com.example.wangchenguang.thelastmile;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.AdapterView;
+
 /**
  * Created by wangchenguang on 15/12/2.
  */
-public class Db {
+public class Db extends SQLiteOpenHelper {
+    public Db(Context context) {
+        super(context, "db", null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE user(" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "commodity TEXT DEFAULT \"\"," +
+                "price TEXT DEFAULT \"\")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/example/wangchenguang/thelastmile/Supermarket.java
+++ app/src/main/java/com/example/wangchenguang/thelastmile/Supermarket.java
@@ -1,31 +1,178 @@
 package com.example.wangchenguang.thelastmile;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ListMenuItemView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
 
-public class Supermarket extends AppCompatActivity {
+public class Supermarket extends ListActivity {
+
+    private SimpleCursorAdapter adapter;
+    private EditText etCommodity, etPrice;
+    private Button btnAdd;
+    private Db db;
+    private SQLiteDatabase dbRead, dbWrite;
+    private View.OnClickListener btnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ContentValues cv = new ContentValues();
+            cv.put("commodity", etCommodity.getText().toString());
+            cv.put("price", etPrice.getText().toString());
+
+            dbWrite.insert("user", null, cv);
+
+            refreshListView();
+
+        }
+    };
+
+
+    private int position;
+    private AdapterView.OnItemClickListener listViewItemLongClickListener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+            new AlertDialog.Builder(Supermarket.this).setNegativeButton("No", null).setTitle("Warning").setMessage("Do you want to Delete it?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Cursor c = adapter.getCursor();
+                    c.moveToPosition(position);
+
+                    int itemId = c.getInt(c.getColumnIndex("_id"));
+                    dbWrite.delete("user", "_id=?", new String[]{itemId + ""});
+                    refreshListView();
+
+                }
+            }).show();
+
+        }
+//
+//        @Override
+//        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//
+//            new AlertDialog.Builder(Supermarket.this).setNegativeButton("No", null).setTitle("Warning").setMessage("Are You Sure?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Cursor c = adapter.getCursor();
+//                    c.moveToPosition(position);
+//
+//                    int itemId = c.getInt(c.getColumnIndex("_id"));
+//                    dbWrite.delete("user","_id=?",new String[]{itemId+""});
+//                    refreshListView();
+//
+//                }
+//            }).show();
+//
+//
+//            return true;
+//        }
+    };
+
+
+//    private AdapterView.OnItemLongClickListener listViewItemLongClickListener = new AdapterView.OnItemLongClickListener() {
+//        @Override
+//        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//
+//            new AlertDialog.Builder(Supermarket.this).setTitle("Warnning").setMessage("Delete it?").setNegativeButton("No", null).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Cursor c = adapter.getCursor();
+//                    c.moveToPosition(position);
+//
+//                    int itemId = c.getInt(c.getColumnIndex("_id"));
+//                    dbWrite.delete("user", "_id=?", new String[]{itemId + ""});
+//                    refreshListView();
+//                }
+//            }).show();
+//
+//
+//            return true;
+//        }
+//    };
+
+//    private View.OnLongClickListener ListViewItemLongClickListener = new View.OnItemLongClickListener() {
+//        @Override
+//        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//
+//            new AlertDialog.Builder(Supermarket.this).setTitle("Warning").setMessage("Are You Sure?").setNegativeButton("No",null).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Cursor c = adapter.getCursor();
+//                    c.moveToPosition(position);
+//
+//                    int itemId = c.getInt(c.getColumnIndex("_id"));
+//                    dbWrite.delete("user","_id=?",new String[]{itemId+""});
+//                    refreshListView();
+//                }
+//            }).show();
+//
+//            return true;
+//        }
+//    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supermarket);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        etCommodity = (EditText) findViewById(R.id.etCommodity);
+        etPrice = (EditText) findViewById(R.id.etPrice);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(btnAddListener);
+
+//        Db db = new Db(this);
+//        SQLiteDatabase dbRead = db.getReadableDatabase();
+//
+        db = new Db(this);
+        dbRead = db.getReadableDatabase();
+        dbWrite = db.getWritableDatabase();
+//        dbRead.close();
+//        dbWrite.close();
+
+
+        adapter = new SimpleCursorAdapter(this, R.layout.dblist, null, new String[]{"commodity", "price"}, new int[]{R.string.app_name, R.id.tvPrice});
+        setListAdapter(adapter);
+
+        refreshListView();
+
+        getListView().setOnItemClickListener((AdapterView.OnItemClickListener) listViewItemLongClickListener);
+
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
+    }
+
+    private void refreshListView() {
+        Cursor c = dbRead.query("user", null, null, null, null, null, null);
+        adapter.changeCursor(c);
     }
 
     @Override
@@ -49,4 +196,8 @@ public class Supermarket extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
+
+
